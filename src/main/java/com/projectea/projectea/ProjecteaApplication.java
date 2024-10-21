8d2907625be08a856a5ce4485feb142ae6e0a1e5
Gill Mertens <gill.mertens@student.ehb.be>
2024-10-22 00:17:09 +0200

    Initial Commit

---
+++ src/main/java/com/projectea/projectea/ProjecteaApplication.java
@@ -0,0 +1,13 @@
+package com.projectea.projectea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecteaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectea/projectea/ProjecteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectea.projectea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
