9362f642bd2ec034cd3db482e783026866be5902
Gill Mertens <gill.mertens@student.ehb.be>
2024-10-26 19:07:26 +0200

    Chore: Setup user entity

---
+++ src/main/java/com/projectea/projectea/domain/impl/user/entities/Address.java
@@ -0,0 +1,21 @@
+package com.projectea.projectea.domain.impl.user.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
--- src/main/java/com/projectea/projectea/domain/impl/user/entities/User.java
+++ src/main/java/com/projectea/projectea/domain/impl/user/entities/User.java
@@ -1,4 +1,38 @@
 package com.projectea.projectea.domain.impl.user.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+
 }
