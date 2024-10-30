4151a937864fdca7db126f3af0a8db393cd84200
Gill Mertens <gill.mertens@student.ehb.be>
2024-10-30 14:26:41 +0100

    Chore: Add Entities for Reservations

---
+++ src/main/java/com/projectea/projectea/domain/impl/reservation/entities/Reservation.java
@@ -0,0 +1,36 @@
+package com.projectea.projectea.domain.impl.reservation.entities;
+
+import com.projectea.projectea.domain.impl.item.entities.Item;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
+    @Column(name = "pickup_date")
+    private LocalDateTime pickupDate;
+
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+    @ManyToMany
+    @JoinTable(
+        name = "reservation_items",
+        joinColumns = @JoinColumn(name = "reservation_id"),
+        inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items;
+}
---
+++ src/main/java/com/projectea/projectea/domain/impl/reservation/entities/ReservationStatus.java
@@ -0,0 +1,9 @@
+package com.projectea.projectea.domain.impl.reservation.entities;
+
+public enum ReservationStatus {
+    PICKED_UP,
+    RETURNED,
+    CANCELLED,
+    PENDING,
+    LATE
+}
